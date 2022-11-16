83b0096c2a8a390d40d1e700db73f3590c03bc3d
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2022-11-16 20:42:14 +0300

    Dz1/Dz2

---
+++ static/py/elements.py
@@ -0,0 +1,29 @@
+tags = [
+    {
+        'name': 'Django',
+        'size': 'fs-5',
+        'color': 'text-warning'
+    },
+    {
+        'name': 'Python',
+        'size': 'fs-4',
+        'color': 'text-danger'
+    },
+    {
+        'name': 'C',
+        'size': 'fs-5',
+        'color': 'text-info'
+    },
+    {
+        'name': 'Java',
+        'size': 'fs-4',
+        'color': 'text-dark'
+    },
+    {
+        'name': 'Rust',
+        'size': 'fs-5',
+        'color': 'text-secondary'
+    }
+]
+
+authorized = False
